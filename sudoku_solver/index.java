2a46d0dbf36fa664e185bff471f0c77d03981c59
Jacob York <95983660+jakeyork25@users.noreply.github.com>
2022-03-26 18:43:20 -0400

    Add files via upload

--- sudoku_solver/index.java
+++ sudoku_solver/index.java
@@ -1,50 +1,102 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 class index {
 
-    public static List<String> ListifyRow(String str) {
-        var row = new ArrayList<String>();
-        for(var i = 0; i < 9; i++) {
-            var digitChar = str.charAt(i);
-            var digit = Character.toString(digitChar);
-            row.add(digit);
-        }       
-        return row;
-    }
+    private static final int GRID_SIZE = 9;
     public static void main(String args[]) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter the top row: ");
-        String firstRow = reader.next();
-        System.out.println("Enter the second row: ");
-        List<String> secondRow = ListifyRow(reader.next());
-        System.out.println("Enter the third row: ");
-        List<String> thirdRow = ListifyRow(reader.next());
-        reader.close();
-        String reversed = firstRow;
-        String[] numArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        ArrayList<String> numList = new ArrayList<>(Arrays.asList(numArray));
-       
+            
+        int[][] board = {
+            {3, 8, 5, 0, 0, 0, 0, 0, 0},
+            {9, 2, 1, 0, 0, 0, 0, 0, 0},
+            {6, 4, 7, 0, 0, 0, 0, 0, 0}, 
+            {0, 0, 0, 1, 2, 3, 0, 0, 0}, 
+            {0, 0, 0, 7, 8, 4, 0, 0, 0}, 
+            {0, 0, 0, 6, 9, 5, 0, 0, 0}, 
+            {0, 0, 0, 0, 0, 0, 8, 7, 3}, 
+            {0, 0, 0, 0, 0, 0, 9, 6, 2}, 
+            {0, 0, 0, 0, 0, 0, 1, 4, 5}
+        };
 
-        for(var i = 0; i < 9; i++) {
-            var digitChar = firstRow.charAt(i);
-            var digit = Character.toString(digitChar);
-            if(digit.equals("0")) {
-                for(String numStr : numList) {
-                    if(!reversed.contains(numStr) && !secondRow.get(i).equals(numStr) && !thirdRow.get(i).equals(numStr)) {
-                        digit = numStr;
-                    }
-                }
-            }
-            reversed = reversed.substring(0,i)+digit+reversed.substring(i + 1);
+        if(solveBoard(board)) {
+            System.out.println("Solved successfully!");
+        } else {
+            System.out.println("Unsolvable board :(");
         }
 
-        System.out.print("The sequence is " + reversed);
+        printBoard(board);
     }
 
-    public static boolean IsInRow(List<String> list) {
+    private static void printBoard(int[][] board) {
+        for(int row = 0; row < GRID_SIZE; row++) {
+            if(row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for(int col = 0; col < GRID_SIZE; col++) {
+                if(col % 3 == 0 && col != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean numberIsInRow(int[][] board, int number, int row) {
+        for(int i = 0; i < GRID_SIZE; i++) {
+            if(board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean numberIsInColumn(int[][] board, int number, int column) {
+        for(int i = 0; i < GRID_SIZE; i++) {
+            if(board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean numberIsInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxCol = column - column % 3;
+
+        for(int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for(int j = localBoxCol; j < localBoxCol + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !numberIsInRow(board, number, row) &&
+        !numberIsInColumn(board, number, column) &&
+        !numberIsInBox(board, number, row, column);
+    }
+
+    private static boolean solveBoard(int[][] board) {
+        for(int row = 0; row < GRID_SIZE; row++) {
+            for(int col = 0; col < GRID_SIZE; col++) {
+                if(board[row][col] == 0) {
+                    for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if(isValidPlacement(board, numberToTry, row, col)) {
+                            board[row][col] = numberToTry;
+
+                            if(solveBoard(board)){
+                                return true;
+                            } else {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
         return true;
     }
+
 }
